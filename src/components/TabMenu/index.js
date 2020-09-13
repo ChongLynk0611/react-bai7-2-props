a463f575e39c048c18b4629729be74eaa7839920
ChongLynk <tronglinhmta0611@gmail.com>
2020-09-13 07:43:42 -0400

    Them cac icon tu mang icon - them the kieu dong

--- src/components/TabMenu/index.js
+++ src/components/TabMenu/index.js
@@ -1,10 +1,29 @@
 import React , {Component} from 'react';
 import './style.css';
+import home from '../../public/home.png';
+import deals from '../../public/gift.png';
+import upload from '../../public/server.png';
+import work from '../../public/food.png';
+import setting from '../../public/settings.png';
 class TabMenu extends Component{
+    
     render(){
+        const icons = [
+            {lable:'HOME' , icon:home},
+            {lable:'DEALS' , icon: deals},
+            {lable:'UPLOAD', icon:upload},
+            {lable:'WORK', icon:work},
+            {lable:'SETTING',icon:setting}
+        ];
         return(
             <div className="TabMenu">
-                <div className="menu-item">
+                {icons.map((icon, index)=>(
+                    <div className="menu-item">
+                    <img src={icon.icon}></img>
+                <span>{icon.lable}</span>
+                </div> 
+                ))}
+                {/* /* <div className="menu-item">
                     <i className="fas fa-home"></i>
                     <span>HOME</span>
                 </div>
@@ -23,7 +42,7 @@ class TabMenu extends Component{
                 <div className="menu-item">
                     <i className="fas fa-cogs"></i>
                     <span>SETTING</span>
-                </div>
+                </div> */ }
             </div>
         );
     }
